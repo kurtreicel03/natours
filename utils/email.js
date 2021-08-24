fdb49ce04acb10e90e369a7aa1e5eb7a3bddd6c8
Kurt Reicel Nunez <kurtreicel@gmail.com>
2021-08-24 21:11:53 +0800

    fixing mail transport

--- utils/email.js
+++ utils/email.js
@@ -16,8 +16,6 @@ module.exports = class Email {
       // Send in Blue
       return nodemailer.createTransport({
         service: 'SendinBlue',
-        host: process.env.SENDIN_BLUE_HOST,
-        port: process.env.SENDIN_BLUE_PORT,
         auth: {
           user: process.env.SENDIN_BLUE_USERNAME,
           pass: process.env.SENDIN_BLUE_PASSWORD,
