55e75174d7c6b111a1e7070f8ef0813d917c6aa7
Kurt Reicel Nunez <kurtreicel@gmail.com>
2021-08-26 15:55:51 +0800

    fixed some bugs

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -67002,7 +67002,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62748" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58262" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
